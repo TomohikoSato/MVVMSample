f8b68b643605f0662187f38be6f95eb0a86f76be
tomohiko sato <tomohikosato1@gmail.com>
2017-12-13 23:58:40 +0900

    MVVM

--- app/src/main/kotlin/com/hayabusa/mvvmsample/login/LoginViewModel.kt
+++ app/src/main/kotlin/com/hayabusa/mvvmsample/login/LoginViewModel.kt
@@ -1,4 +1,12 @@
 package com.hayabusa.mvvmsample.login
 
+import io.reactivex.subjects.PublishSubject
+
 class LoginViewModel {
+
+    val showProgress = PublishSubject.create<Void>()
+    val dismissProgress = PublishSubject.create<Void>()
+    val moveToLoginCompleteActivity = PublishSubject.create<Void>()
+
+
 }
